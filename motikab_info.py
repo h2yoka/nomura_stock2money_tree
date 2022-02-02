039237aebf8d3afd7f254c6f4eb67652150d7ed0
h2yoka <h2yoka.main@gmail.com>
2022-02-02 21:19:18 +0900

    Add main code

---
+++ motikab_info.py
@@ -0,0 +1,101 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+email_nomura = "#########@######"
+pass_nomura = "XXXXXXXXXXXXXXXXX"
+
+email_money_t = "#########@######"
+pass_money_t = "XXXXXXXXXXXXXXXXX"
+
+options = webdriver.ChromeOptions()
+# options.add_argument('-headless')
+
+browser = webdriver.Chrome(options=options)
+
+# ---------Nomura Employee Stock Site-------
+browser.get("https://www.e-plan.nomura.co.jp/login/index.html")
+sleep(1.0)
+
+mail = browser.find_element(by=By.NAME, value="m_login_mail_address")
+
+password = browser.find_element(by=By.NAME, value="WEAPASS")
+
+mail.clear()
+password.clear()
+
+mail.send_keys(email_nomura)
+password.send_keys(pass_nomura)
+
+nomura_login = browser.find_element(by=By.CLASS_NAME, value="m_login_btn_01")
+
+#login
+nomura_login.submit()
+sleep(2.0)
+
+#時価評価額を取得する
+now_stock_element = browser.find_element(by=By.XPATH, value='//*[@id="container"]/div[2]/div[2]/dl/dd/span[2]')
+stock_value = (now_stock_element.text).replace(",","")
+stock_value = int(stock_value)
+print(stock_value)
+
+
+# #ログアウト
+logout_element = browser.find_element(by=By.XPATH, value='//*[@id="m_self_reissue"]/ul/li[3]/a/span[2]')
+logout_element.click()
+
+# sleep(1.0)
+
+# money treeにログインする
+# url : https://myaccount.getmoneytree.com/login?locale=ja
+browser.get("https://app.getmoneytree.com/login")
+
+#URLが遷移する処理が入るので、次の処理はすぐに行わない
+sleep(2.0)
+
+mt_email_input = browser.find_element(by=By.NAME, value="guest[email]")
+mt_email_input.send_keys(email_money_t)
+
+mt_pass_input = browser.find_element(by=By.NAME, value="guest[password]")
+mt_pass_input.send_keys(pass_money_t)
+
+mt_login_button = browser.find_element(by=By.CLASS_NAME, value="login-form-button")
+mt_login_button.submit()
+
+#ログイン処理
+sleep(5.0)
+
+# 口座残高画面に移動する
+browser.get("https://app.getmoneytree.com/app/vault")
+sleep(5.0)
+
+#指定した口座を左カラムから選択
+target_category_element = browser.find_element(by=By.XPATH, value='//*[@id="mt-webapp"]/section/mt-webapp-layout/div/div[2]/mt-accounts/div/mt-accounts-personal/div/mt-link-vault/div/mt-two-column-layout/div/div[1]/div[2]/left-column-body/mt-credentials/div/ul/li[4]')
+target_category_element.click()
+sleep(0.5)
+
+target_bank_element = browser.find_element(by=By.XPATH, value='//*[@id="mt-webapp"]/section/mt-webapp-layout/div/div[2]/mt-accounts/div/mt-accounts-personal/div/mt-link-vault/div/mt-two-column-layout/div/div[1]/div[2]/left-column-body/mt-credentials/div/ul/li[4]/div[2]/ul/li[1]/div/mt-credential/div/mt-credential-template/div/div[3]/div[2]/mt-list-secondary/div/ul/li[2]')
+target_bank_element.click()
+sleep(0.5)
+
+bank_value_element = browser.find_element(by=By.CSS_SELECTOR, value=".balance.ng-binding")
+bank_value_element.click()
+
+exp_inc_switch_element = browser.find_element(by=By.CSS_SELECTOR, value=".icon-minus-circle.ng-scope")
+exp_inc_switch_element.click()
+
+banck_amount_element = browser.find_element(by=By.CLASS_NAME, value="input-amount")
+banck_amount_element.clear()
+banck_amount_element.send_keys(stock_value)
+
+update_element = browser.find_element(by=By.CSS_SELECTOR, value=".update.ng-scope")
+update_element.click()
+
+#ログアウト処理
+mt_setting_element = browser.find_element(by=By.CLASS_NAME, value="icon-thin-cog")
+mt_setting_element.click()
+mt_logout_element = browser.find_element(by=By.CSS_SELECTOR, value=".logout.ng-scope")
+mt_logout_element.click()
+
+browser.close()
